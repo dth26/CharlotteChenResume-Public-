0cb9ba45ddff8d9d3887758fab006871a69383b9
pythonprogrammer <dth26@pitt.edu>
2015-06-25 19:03:54 +0000

    fix index page

--- static/js/index.js
+++ static/js/index.js
@@ -1,22 +1,21 @@
 	var height = $(window).height();
 
-	$(document).ready(function(){
-		var menuHeight = $('#menu').height();
-		$('body').css('overflow','hidden');
+$(document).ready(function(){
+	var menuHeight = $('#menu').height();
+	$('body').css('overflow','hidden');
 
-		$("#homeMenuItem").addClass("menuItemSelected");
-		$("#homeMenuItem").addClass("homeSettings");
-
-		$("#circleIndex").css("background-color","#576F94");
-
-		$("#homeMenuItem img").css("display","inline");
-	});
+    /* highlight appropriate menu items for index page */
+	$("#homeMenuItem").addClass("menuItemSelected");
+	$("#homeMenuItem").addClass("homeSettings");
+	$("#circleIndex").css("background-color","#576F94");
+	$("#homeMenuItem img").css("display","inline");
+});
 
 
 
 
 /* very last function called after document loads*/
-/* set position of image on index page */
+/* set position of image on index page, ensures image does not overlap menu */
 (function($) {
     $(window).bind("load", function() {
     	var docHeight = $(document).height() + 'px';
