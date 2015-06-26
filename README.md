7529a89a81d820272fa3428c6e8dbb3f941bc585
pythonprogrammer <dth26@pitt.edu>
2015-06-26 18:41:00 +0000

    build welcome page

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 app.py          - contains actual flask app that initiates program, the first module that is called.
 config.py       - connects flask app to sqlite3 database by declaring the path to the db_file.db file
 db_create.py    - creates the database in a file called db_file.db
+__init__.py     - contains connection to database called 'db'. Must be imported by all models
 templates       - these are the actual html files that construct the layout of the page 'file.html'
 models          - contains data for structure and creation of database tables, data manipulation, and data pulls.
                     a) Any new models should be imported into models.py
---
+++ static/js/welcome.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('#next').click(function(){
+        var currentCenter = $('#currentCenter').attr('value');
+
+
+        if(currentCenter == "one"){
+            $('#one').fadeOut(1000, function(){
+                $('#two').fadeIn(1500);
+            });
+
+            $('#currentCenter').attr('value','two');
+        }else if(currentCenter == "two"){
+            $('#two').fadeOut(1000, function(){
+                $('#three').fadeIn(1500);
+            });
+            $('#currentCenter').attr('value','three');
+        }else if(currentCenter == "three"){
+           $('#three').fadeOut(1000, function(){
+                $('#four').fadeIn(1500);
+            });
+            $('#currentCenter').attr('value','four');
+        }
+        else if(currentCenter == "four"){
+            var url = "http://pythonprogrammer.pythonanywhere.com/";
+ 		    window.open(url, '_self' );
+        }
+    });
+});
